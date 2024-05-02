0eb9d78eb96b05e79df51c79b5088fcd29eee94a
enochmakafui <108337287+enochmakafui@users.noreply.github.com>
2024-05-02 11:31:58 +0000

    build increase and decrease cart item quantity

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,26 +1,36 @@
 import { useContext } from "react";
 import { CartContext } from "../store/CartContext";
+
 export default function Cart({ onCloseModal }) {
-  const { orderItems } = useContext(CartContext);
+  const { orderItems, removeFromCart, increaseItemQuantity } =
+    useContext(CartContext);
+  function calculateItemTotal(order) {
+    const price = parseFloat(order.price);
+    return order.quantity * price;
+  }
+
+  const totalPrice = orderItems.reduce((total, order) => {
+    return total + calculateItemTotal(order);
+  }, 0);
   return (
     <div className="cart">
       <h2>Your Cart</h2>
-      {orderItems.length === 0 && <p>empty cart</p>}
+      {orderItems.length === 0 && <p>cart is empty</p>}
       {orderItems.map((order) => (
         <ul key={order.id} className="cart-item">
           <li>
             <p>
-              {order.name} - {order.quantity || 1} x ${order.price}
+              {order.name} - {order.quantity} x ${order.quantity * order.price}
             </p>
           </li>
           <li className="cart-item-actions">
-            <button>-</button>
-            <span>{order.quantity || 1}</span>
-            <button>+</button>
+            <button onClick={() => removeFromCart(order.id)}>-</button>
+            <span>{order.quantity}</span>
+            <button onClick={() => increaseItemQuantity(order.id)}>+</button>
           </li>
         </ul>
       ))}
-      <div className="cart-total">Total</div>
+      <div className="cart-total">${totalPrice.toFixed(2)}</div>
       <div className="modal-actions">
         <button className="text-button" onClick={() => onCloseModal()}>
           close
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -4,16 +4,61 @@ export const CartContext = createContext({
   orderItems: [],
   addToCart: () => {},
   removeFromCart: () => {},
+  increaseItemQuantity: () => {},
 });
 function cartReducer(state, action) {
   if (action.type === "ADD_ITEM") {
+    const itemExists = state.orders.some(
+      (order) => order.id === action.meal.id,
+    );
+    if (itemExists) {
+      return {
+        orders: state.orders,
+      };
+    }
+    action.meal.quantity = 1;
     const updatedOrders = [...state.orders, action.meal];
-    console.log(action.meal);
+    return {
+      orders: updatedOrders,
+    };
+  }
+  if (action.type === "INCREASE_QUANTITY") {
+    const updatedOrders = state.orders.map((order) => {
+      if (order.id === action.id) {
+        return {
+          ...order,
+          quantity: order.quantity + 1,
+        };
+      }
+      return order;
+    });
     return {
       orders: updatedOrders,
     };
   }
   if (action.type === "DELETE_ITEM") {
+    const item = state.orders.find((order) => order.id === action.id);
+    if (item && item.quantity === 1) {
+      const updatedOrders = state.orders.filter(
+        (order) => order.id !== action.id,
+      );
+      return {
+        orders: updatedOrders,
+      };
+    } else if (item) {
+      const updatedOrders = state.orders.map((order) => {
+        if (order.id === action.id) {
+          return {
+            ...order,
+            quantity: order.quantity - 1,
+          };
+        }
+        return order;
+      });
+      return {
+        orders: updatedOrders,
+      };
+    }
   }
 }
 
@@ -33,10 +78,17 @@ export default function CartContextProvider({ children }) {
       id: id,
     });
   }
+  function handleIncreaseItemQuantity(id) {
+    cartDispatch({
+      type: "INCREASE_QUANTITY",
+      id: id,
+    });
+  }
   const ctxValues = {
     orderItems: cartState.orders,
     addToCart: handleAddItemToCart,
     removeFromCart: handleDeleteFromCart,
+    increaseItemQuantity: handleIncreaseItemQuantity,
   };
   return (
     <CartContext.Provider value={ctxValues}>{children}</CartContext.Provider>
